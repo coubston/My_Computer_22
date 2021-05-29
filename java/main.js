739902744a63c6403b8afc4326a6e82078607016
coubston <54748503+coubston@users.noreply.github.com>
2021-05-29 15:05:04 +0700

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,42 @@
+$("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
+$(function(){
+    $('.slider').slick({
+        autoplay:true,
+        dots: true,
+        infinite:true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    })
+})
